5edbef55749f959d258424688d1c5251d02b097b
Ron Le <ronle9519@gmail.com>
2022-08-12 22:33:47 +0700

    singleton

---
+++ Singleton Pattern/Database.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Singleton_Pattern
+{
+    public class Database
+    {
+        private static readonly object lockObject = new object();
+        private static Database db;
+        private List<string> data;
+        private Database()
+        {
+            data = new List<string>() 
+            { 
+                "Ron",
+                "Le",
+                "Manh"
+            };
+        }
+
+        public static Database getInstance()
+        {
+            // Lazy Initialization
+            if (db == null)
+            {
+                db = new Database();
+            }
+
+            /*
+            Thread safe
+            lock (lockObject)
+            {
+                if (db == null)
+                {
+                    db = new Database();
+                }
+            }
+            */
+
+            /* 
+            Double check
+            if (db == null)
+            {
+                lock (lockObject)
+                {
+                    if (db == null)
+                    {
+                        db = new Database();
+                    }
+                }
+            }
+            */
+            return db;
+        }
+
+        public List<string> query(Func<string, bool> expression)
+        {
+            return data.Where(expression).ToList();
+        }
+    }
+}
---
+++ Singleton Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Singleton_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string data in Database.getInstance().query(x => x.Contains("R")))
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
